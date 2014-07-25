80c929f30ea255feff358c672cc4e62627dcfa37
Vochescu Alexandru <valex@valexandru.biz>
2014-07-25 12:48:43 +0300

    Update db_config.php

--- android_connect/db_config.php
+++ android_connect/db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
 
-define('DB_USER', "hackaton"); // db user
-define('DB_PASSWORD', "facebook"); // db password (mention your db password here)
+define('DB_USER', "*****"); // db user
+define('DB_PASSWORD', "******"); // db password (mention your db password here)
 define('DB_DATABASE', "smartflyer"); // database name
 define('DB_SERVER', "localhost"); // db server
 ?>
